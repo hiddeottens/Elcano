ad65a1153edf4bedfae13b3866071a9ecc208255
Hidde Ottens <hidde95@gmail.com>
2020-10-08 18:01:59 +0200

    Set up eslint / prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,60 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
     },
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+      'plugin:prettier/recommended',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'plugin:import/typescript',
     ],
-    "rules": {
-    }
-};
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          moduleDirectory: ['node_modules', '/'],
+        },
+      },
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+      'react/prop-types': 0,
+      'react/jsx-props-no-spreading': 0,
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+      'import/extensions': [
+        "error",
+        "ignorePackages",
+        {
+          "js": "never",
+          "jsx": "never",
+          "ts": "never",
+          "tsx": "never"
+        }
+     ]
+    },
+    overrides: [
+      {
+        files: ['*.ts', '*.tsx'],
+        rules: {
+          '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
+        }
+      }
+    ]
+  };
+  
