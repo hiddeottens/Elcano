6323d247f772f97511f87f9aba6c66cf61c4c03b
Hidde Ottens <hidde95@gmail.com>
2021-03-28 19:03:30 +0200

    fix: remove unnecessary import

--- src/side-slide/SideSlide.tsx
+++ src/side-slide/SideSlide.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { AnimatePresence } from 'framer-motion';
-import { RoundButton } from '../round-button';
 import { SideSlideBody, CloseButtonContainer, CloseButton } from './styles';
 import { Backdrop } from '../backdrop';
 
