53f595ee57cf4c9ab0fce6f2c134b2153736b3d2
Hidde Ottens <hidde95@gmail.com>
2021-04-06 23:12:24 +0200

    chore: bump emotion to v11.0.0

--- src/spinner/Spinner.tsx
+++ src/spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
-import { keyframes } from '@emotion/core';
+import { keyframes } from '@emotion/react';
 
 export interface SpinnerProps {
   color?: string;
